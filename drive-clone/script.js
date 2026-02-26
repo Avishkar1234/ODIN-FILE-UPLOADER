b76dd0ccf3a43164453e6c68eeb276b047746ab1
Avishkar <avishkar1234567890@gmail.com>
2026-02-26 12:55:33 +0530

    Fix Prisma 7 adapter configuration and enable ESM support

---
+++ drive-clone/script.js
@@ -0,0 +1,16 @@
+import { prisma } from './lib/prisma.js';
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      email: "test@test.com",
+      password: "1234"
+    }
+  });
+
+  console.log(user);
+}
+
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
